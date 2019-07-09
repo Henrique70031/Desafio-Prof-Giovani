8c027fa130045fdec75d6635fae3f19c9cde5ac0
Henrique70031 <Henrique70031@users.noreply.github.com>
2019-07-09 14:42:21 -0300

    Strings em aspas duplas

--- desafio.py
+++ desafio.py
@@ -68,11 +68,11 @@ contagemFuncionarios = contadorFuncionarios(dicionarios);
 mediaSalarios, maiorSalario = pesquisaSalarios(dicionarios, contagemFuncionarios);
 
 informacoes = {
-    'num_funcionarios': contagemFuncionarios,
-    'media_salarios': mediaSalarios,
-    'maior_salario': maiorSalario,
-    'dicionarios': dicionarios,
-    'lista_desordenada': lista_desordenada
+    "num_funcionarios": contagemFuncionarios,
+    "media_salarios": mediaSalarios,
+    "maior_salario": maiorSalario,
+    "dicionarios": dicionarios,
+    "lista_desordenada": lista_desordenada
 }
 
 rodarMenu(informacoes);
