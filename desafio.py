9ae05a4f06e451e6de396e41b5f580c444fc4041
Henrique70031 <Henrique70031@users.noreply.github.com>
2019-07-09 14:39:55 -0300

    Comentários e strings com aspas duplas

--- desafio.py
+++ desafio.py
@@ -3,53 +3,63 @@ from menu import rodarMenu;
 def contadorFuncionarios(lista_dicionarios): # Conta quantos funcionários existem
     count = 0;
 
+def contadorFuncionarios(lista_dicionarios):
+    """ Conta quantos funcionários existem"""
+
+    count = 0
+
     for nome in lista_dicionarios:
-        count += 1;
-    return count;
+        count += 1
+    return count
 
 def criarDicionario():
-    arquivo = open('remuneracao.txt', 'r');
-    lista_dic_funcionarios = [];
+    arquivo = open("remuneracao.txt", "r")
+    lista_dic_funcionarios = []
 
     for linha in arquivo:
-        linha = linha.replace('\n', '');
-        lista_linha = linha.split(';');
+        linha = linha.replace("\n", "")
+        lista_linha = linha.split(";")
         lista_dic_funcionarios.append({
-            'nome': lista_linha[0],
-            'cargo': lista_linha[1],
-            'órgão': lista_linha[2],
-            'remuneracao_do_mês': lista_linha[3],
-            'ferias_e_13_salário': lista_linha[4],
-            'pagamentos_eventuais': lista_linha[5],
-            'licença_prêmio_indenizada': lista_linha[6],
-            'abono_permanêncica_e_outras_indenizações': lista_linha[7],
-            'redutor_salarial': lista_linha[8],
-            'total_líquido': lista_linha[9]
-        });
-    arquivo.close();
-    lista_dic_funcionarios.pop(0);
-    return lista_dic_funcionarios;
+            "nome": lista_linha[0],
+            "cargo": lista_linha[1],
+            "órgão": lista_linha[2],
+            "remuneracao_do_mês": lista_linha[3],
+            "ferias_e_13_salário": lista_linha[4],
+            "pagamentos_eventuais": lista_linha[5],
+            "licença_prêmio_indenizada": lista_linha[6],
+            "abono_permanêncica_e_outras_indenizações": lista_linha[7],
+            "redutor_salarial": lista_linha[8],
+            "total_líquido": lista_linha[9]
+        })
+    arquivo.close()
+    lista_dic_funcionarios.pop(0)
+    return lista_dic_funcionarios
 
 def pesquisaSalarios(lista_dicionarios, num_funcionarios):
-    soma_salarios = 0;
-    maior_salario = 0;
+    """ Retorna a média dos salários e o maior salário"""
+
+    soma_salarios = 0
+    maior_salario = 0
 
     for pessoa in lista_dicionarios:
         # Troca ',' por '.', assim pode ser transformado para float
-        salario_atual = float(pessoa['total_líquido'].replace(',', '.'));
+        salario_atual = float(pessoa["total_líquido"].replace(",", "."))
         if (salario_atual > maior_salario): # Pega o maior salario
-            maior_salario = salario_atual;
-        soma_salarios += salario_atual;
+            maior_salario = salario_atual
+        soma_salarios += salario_atual
     # Media arredondada
-    media_salarios = round(soma_salarios / num_funcionarios, 2);
-    return media_salarios, maior_salario;
+    media_salarios = round(soma_salarios / num_funcionarios, 2)
+    return media_salarios, maior_salario
 
 def criarListaDesordenada(dicionarios):
-    lista = [];
+    """ Cria uma lista com salários para testar a velocidade dos
+        ordenadores bubblesort, selectionsort e mergesort.
+    """
+    lista = []
     for index in range(10000):
-        salario_atual = dicionarios[index]['remuneracao_do_mês'].replace(',', '.');
-        lista.append(float(salario_atual));
-    return lista;
+        salario_atual = dicionarios[index]["remuneracao_do_mês"].replace(",", ".")
+        lista.append(float(salario_atual))
+    return lista
 ###################################################
 
 dicionarios = criarDicionario();
